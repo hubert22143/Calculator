c7532d25acaf1028824327ec40f5489d6f2cb309
Hubert Baciak <hubert.baciak@o2.pl>
2023-08-15 18:15:55 +0200

    Changed index , style structure. Still working on script to work.

--- script.js
+++ script.js
@@ -1,31 +1,72 @@
 document.addEventListener('DOMContentLoaded', (e) => {
     const input = document.getElementById('input');
     input.value = '';
-
+    let currentValue = '';
     input.addEventListener('input', () => {
         input.value = input.value.replace(/[^\d+\-*\/%]/g, '');
+        currentValue = input.value;
     })
-    const addToInputNumberValues = (clickedValue) => {
-        if (clickedValue !== "c") {
-            input.value += clickedValue;
-        }
-    }
-
-
-
-    const bottomContent = document.querySelector('.bottom-content');
-    const numberButtons = bottomContent.querySelectorAll('[id]');
+    const numberButtons = Array.from(document.querySelectorAll('.calc'));
     numberButtons.forEach(button => {
-        button.addEventListener("click", (e) => {
-            addToInputNumberValues(e.target.textContent);
-        });
-    });
+        button.addEventListener('click', (e) => {
+            currentValue += e.target.textContent;
+            input.value = currentValue;
+        })
+    })
+    const operators = {
+        '+': (currentValue,previousValue) => currentValue + previousValue  ,
+        '-': (currentValue,previousValue) => currentValue - previousValue ,
+        '/': (currentValue,previousValue) => currentValue / previousValue,
+        '%': (currentValue,previousValue) => currentValue % previousValue,
+        '*': (currentValue,previousValue) => currentValue*previousValue,
+        '=': (currentValue,previousValue) => currentValue,
+    };
+    const operatorButtons = Array.from(document.querySelectorAll('.operator-button'));
+    let previousValue = 0;
+    operatorButtons.forEach(operator => { 
+        operator.addEventListener("click", () => {
+            if(currentValue !== ''){
+                const operatorSymbol = operator.textContent
+                console.log(operatorSymbol)
+                console.log("currentValue:" + currentValue)
+                previousValue = operators[operatorSymbol](parseFloat(currentValue), parseFloat(previousValue));
+                console.log("previousValue:"+previousValue)
+                input.value = previousValue;
+
+                currentValue = '';
+            }
+        })
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
     const clearButton = document.getElementById("clear");
     clearButton.addEventListener("click", function() {
         input.value = "";
+        currentValue = '';
+        previousValue = '';
+        selectedOperator = null;
     });
     const eraseOneChar = document.getElementById("erase");
     eraseOneChar.addEventListener("click", () => {
@@ -37,5 +78,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
         document.execCommand("Copy");
         alert("Copied");
     });
+
+
 
 });
